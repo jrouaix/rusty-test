f465a2abc827651dde5078714518d8b6c0b2b27d
Jêrome Rx <jerome+git@rouaix.eu>
2019-02-05 08:35:02 +0100

    STEP1 OK !

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,52 @@ extern crate csv;
 use std::error::Error;
 use std::io;
 use std::process;
+#[macro_use]
+extern crate serde_derive;
+
+
+#[derive(Debug, Deserialize)]
+struct Record {
+    column: Option<String>,
+    columnA: Option<String>, 
+    columnB: Option<String>,
+    columnC : Option<i64>,
+    // column_c : String,
+    columnD: Option<i64>,
+    otherColumn: Option<String>
+}
+
+
 
 fn example() -> Result<(), Box<Error>> {
     // Build the CSV reader and iterate over each record.
-    let mut rdr = csv::Reader::from_reader(io::stdin());
-    for result in rdr.records() {
-        // The iterator yields Result<StringRecord, Error>, so we check the
-        // error here.
-        let record = result?;
-        println!("{:?}", record);
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(b';')
+        .flexible(true)
+        .from_reader(io::stdin());
+
+    for result in rdr.deserialize() {
+        if let Err(_) = result { continue }
+        let record: Record = result?;
+
+        // println!("{:?}", record);
+
+        match record.columnC {
+            None    => continue,
+            Some(c) => {
+                match record.columnD {
+                    None    => continue,
+                    Some(d) => {
+                        let sum = c + d;
+                        if sum > 100 {
+                            println!("{}{}", c, d);
+                        }
+                    }
+                }
+            },
+        }
     }
+
     Ok(())
 }
 
