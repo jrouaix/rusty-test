492590cdf35f65fc9687575434812447abe7972a
Jêrome Rx <jerome+git@rouaix.eu>
2019-02-05 09:02:08 +0100

    better pattern matching

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,14 @@ use std::process;
 #[macro_use]
 extern crate serde_derive;
 
+/*
+
+Replace the output from the previous step. Write a big JSON array of objects for the previous matched lines:
+- Rows that can be processed correctly : { "lineNumber": <FILE_LINE_NUMBER>, "type": "ok", "concatAB": "<PREVIOUS_AB_CONCAT>", "sumCD": <PREVIOUS_CD_SUM> }
+- Rows that can't be processed correctly : { "lineNumber": <FILE_LINE_NUMBER>, "type": "error", "errorMessage": <ERROR_MESSAGE> }
+
+*/
+
 
 #[derive(Debug, Deserialize)]
 struct Record {
@@ -32,21 +40,24 @@ fn example() -> Result<(), Box<Error>> {
         let record: Record = result?;
 
         // println!("{:?}", record);
-
-        match record.columnC {
-            None    => continue,
-            Some(c) => {
-                match record.columnD {
-                    None    => continue,
-                    Some(d) => {
-                        let sum = c + d;
-                        if sum > 100 {
-                            println!("{}{}", c, d);
-                        }
-                    }
-                }
-            },
+        if let (Some(a), Some(b), Some(c), Some(d)) = (record.columnA, record.columnB, record.columnC, record.columnD) 
+        {
+            let sum = c + d;
+            if sum > 100 {
+                println!("{}{}", a, b);
+            }
         }
+
+        // THIS MATCHING DOES NOT WORK :-/
+        // match (record.columnA, record.columnB, record.columnC, record.columnD) {
+        //     _    => continue,
+        //     (Some(a), Some(b), Some(c), Some(d)) => {
+        //         let sum = c + d;
+        //         if sum > 100 {
+        //             println!("{}{}", a, b);
+        //         }
+        //     },
+        // }
     }
 
     Ok(())
