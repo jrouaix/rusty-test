c280da3d27d8d2b7183b3976f9350a6a94929b3c
Jêrome Rx <jerome+git@rouaix.eu>
2019-02-10 00:13:38 +0100

    changed again

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,11 @@ extern crate serde_derive;
 // use std::env;
 // env::args()
 
-/*
-
-Replace the output from the previous step. Write a big JSON array of objects for the previous matched lines:
-- Rows that can be processed correctly : { "lineNumber": <FILE_LINE_NUMBER>, "type": "ok", "concatAB": "<PREVIOUS_AB_CONCAT>", "sumCD": <PREVIOUS_CD_SUM> }
-- Rows that can't be processed correctly : { "lineNumber": <FILE_LINE_NUMBER>, "type": "error", "errorMessage": <ERROR_MESSAGE> }
-
-*/
+// Output format (plain text, JSON, etc.) can now be configured by command line arguments. Integrate at least one new format.
 
 
-
-
-fn example() -> Result<(), Box<Error>> {
+fn main() {
+    
     let input = io::stdin();
     //let handle = input;
     let handle = input.lock(); // change nothing after all
@@ -49,20 +42,24 @@ fn example() -> Result<(), Box<Error>> {
         count += 1;
 
         match result {
-            Err(err) => {
-                let output = formater.format_error_line(&count, &err);
-
-                let mut jsonline = if count > 1 { String::from(", ") } else { String::from("") }; 
-                jsonline.push_str(&serde_json::to_string(&output)?);
-                println!("{}", jsonline);
-            }
-            Ok(_) => {
-                let record: SourceLine = result?;
+            Ok(content) => {
+                let record: SourceLine = content;
                 if let (Some(c), Some(d)) = (record.column_c, record.column_d) 
                 {
                     let sum = c + d;
                     if sum > 100 {
-                        let output = formater.format_ok_line(&count, &record);
+                        let empty = String::default();
+                        let a = record.column_a.as_ref().unwrap_or(&empty);
+                        let b = record.column_b.as_ref().unwrap_or(&empty);
+                        
+                        let output = OkLineOutput {
+                            line_number: count,
+                            line_type: String::from("ok"),
+                            concat_ab: format!("{}{}", a, b),
+                            sum_cd: record.column_c.unwrap() + record.column_c.unwrap(),
+                        };
+
+                        let output = formater.format_ok_line(&count, &output);
                         
                         let mut jsonline = if count > 1 { String::from(line_separator) } else { String::default() }; 
                         jsonline.push_str(&output);
@@ -70,19 +67,23 @@ fn example() -> Result<(), Box<Error>> {
                     }
                 }
             }
+            Err(err) => {
+                let output = ErrorLineOutput {
+                    line_number: count,
+                    line_type: String::from("error"),
+                    error_message: format!("{:?}", err),
+                };
+
+                let output = formater.format_error_line(&count, &output);
+
+                let mut jsonline = if count > 1 { String::from(", ") } else { String::from("") }; 
+                jsonline.push_str(&output);
+                println!("{}", jsonline);
+            }
         }
     }
 
     println!("{}", formater.get_output_end());
-
-    Ok(())
-}
-
-fn main() {
-    if let Err(err) = example() {
-        println!("error running example: {}", err);
-        process::exit(1);
-    }
 }
 
 // ***************************************************
@@ -90,29 +91,8 @@ fn main() {
 // ***************************************************
 
 
-trait OutputFormater {
-    
-    fn format_result<E: Error>(&self, line_number : &i32,  result: &Result<SourceLine, E>) -> String
-    {
-        match result{
-            Ok(line) => self.format_ok_line(line_number, &line),
-            Err(err) => self.format_error_line(line_number, err)
-        }
-    }
-
-    fn format_ok_line(&self, line_number : &i32, line: & SourceLine) -> String;
-    fn format_error_line<E: Error>(&self, line_number : &i32,  err: &E) -> String;
-
-    fn get_line_separator(&self) -> &'static str;
-    fn get_output_begin(&self) -> &'static str;
-    fn get_output_end(&self) -> &'static str;
-}
-
-// Json
-
-
 #[derive(Debug, Serialize)]
-struct JsonOkLineOutput {
+struct OkLineOutput {
     #[serde(rename = "lineNumber")]
     line_number: i32,
     #[serde(rename = "lineType")]
@@ -124,7 +104,7 @@ struct JsonOkLineOutput {
 }
 
 #[derive(Debug, Serialize)]
-struct JsonErrorLineOutput {
+struct ErrorLineOutput {
     #[serde(rename = "lineNumber")]
     line_number: i32,
     #[serde(rename = "lineType")]
@@ -133,36 +113,34 @@ struct JsonErrorLineOutput {
     error_message: String,
 }
 
+
+trait OutputFormater {
+    
+    fn format_ok_line(&self, line_number : &i32, line: &OkLineOutput) -> String;
+    fn format_error_line(&self, line_number : &i32,  err: &ErrorLineOutput) -> String;
+
+    fn get_line_separator(&self) -> &'static str;
+    fn get_output_begin(&self) -> &'static str;
+    fn get_output_end(&self) -> &'static str;
+}
+
+// Json
+
+
+
 struct JsonOutputFormater 
 {
 
 }
 
 impl OutputFormater for JsonOutputFormater{
-    fn format_ok_line(&self, line_number : &i32, line: &SourceLine) -> String{
-        let empty = String::default();
-        let a = line.column_a.as_ref().unwrap_or(&empty);
-        let b = line.column_b.as_ref().unwrap_or(&empty);
-        
-        let output = JsonOkLineOutput {
-            line_number: *line_number,
-            line_type: String::from("ok"),
-            concat_ab: format!("{}{}", a, b),
-            sum_cd: line.column_c.unwrap() + line.column_c.unwrap(),
-        };
-
-        serde_json::to_string(&output)
+    fn format_ok_line(&self, line_number : &i32, line: &OkLineOutput) -> String{
+        serde_json::to_string(line)
             .unwrap_or_else(|e| panic!(e)) // should not happen
     }
 
-    fn format_error_line<E: Error>(&self, line_number : &i32,  err: &E) -> String{
-        let output = JsonErrorLineOutput {
-            line_number: *line_number,
-            line_type: String::from("error"),
-            error_message: format!("{:?}", err),
-        };
-
-        serde_json::to_string(&output)
+    fn format_error_line(&self, line_number : &i32,  err: &ErrorLineOutput) -> String{
+        serde_json::to_string(err)
             .unwrap_or_else(|e| panic!(e)) // should not happen
     }
 
